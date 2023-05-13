d64259afbbd5e0945eea04a8185566c854443b8c
nurulkhairina00 <nurulkhairina00@gmail.com>
2023-05-13 21:56:40 +0700

    penambahan moment dan perubahan image

--- src/jsPDF-AutoTable/index.js
+++ src/jsPDF-AutoTable/index.js
@@ -2,25 +2,11 @@ import React, { useEffect, useState } from "react";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import axios from "axios";
+import moment from "moment/moment";
 export default function Index() {
   let [data, setData] = useState([]);
   let [hasil, setHasil] = useState([]);
-  let [image, setImage] = useState("");
   let temp = [];
-
-  const getBase64Image = async (url) => {
-    let data = await fetch(url);
-    let blob = await data.blob();
-    return new Promise((resolve) => {
-      let reader = new FileReader();
-      reader.readAsDataURL(blob);
-      reader.onload = () => {
-        let base64data = reader.result;
-        resolve(base64data);
-        setImage(base64data);
-      };
-    });
-  };
 
   const isi = () => {
     temp = [];
@@ -42,20 +28,27 @@ export default function Index() {
       const doc = new jsPDF("p", "pt", "a4");
       doc.autoTable({ html: "#my-table" });
       let pageWidth = doc.internal.pageSize.getWidth();
+
       for (let i = 0; i < data.length; i++) {
-        doc.addImage(image, "PNG", 20, 10, 70, 65);
-        doc.setFontSize(12);
+        doc.addImage("/karyawan.png", "PNG", 20, 10, 70, 65);
+        doc.setFontSize(16);
         doc.setFont(undefined, "bold");
         doc.text("COMPANY CIPTA ADIL", pageWidth / 2, 40, "center");
-        doc.setFontSize(11);
+
+        doc.setFontSize(14);
         doc.setFont(undefined, "bold");
         doc.text("EMPLOYEE DATA", pageWidth / 2, 60, "center");
+
         doc.line(10, 85, pageWidth - 10, 85);
         doc.line(10, 87, pageWidth - 10, 87);
+
+        doc.setFontSize(10);
+        doc.setFont(undefined, "normal");
+        doc.text("Tanggal cetak : " + moment().format("DD MMMM YYYY"), pageWidth - 140, 103);
         doc.autoTable({
           theme: "grid",
-          margin: { top: 95, horizontal: 10 },
-          startY: 95,
+          margin: { top: 115, horizontal: 10 },
+          startY: 115,
           headStyles: {
             minCellHeight: 10,
             valign: "middle",
@@ -70,7 +63,7 @@ export default function Index() {
             3: { cellWidth: 130, halign: "left" },
             4: { cellWidth: 120, halign: "left" },
             5: { cellWidth: 90, halign: "left" },
-          }, // Cells in first column centered and green
+          },
           didPareCell: function (cell) {
             console.log(cell);
             if (cell.row.section === "head") {
@@ -84,10 +77,32 @@ export default function Index() {
               cell.cell.styles.lineWidth = 0.1;
             }
           },
+          // didDrawCell: (cell) => {
+          //   if (cell.row.text[0] === "") {
+          //     let img = `http://localhost:3000/karyawan.png`;
+          //     doc.addImage(img, "PNG", cell.cell.x + 10, cell.cell.y + 10, 180, 30);
+          //   }
+          // },
           body: hasil,
         });
 
-        window.open(doc.output("bloburl"), "_blank");
+        doc.setFontSize(10);
+        doc.setFont(undefined, "normal");
+        doc.text("Demikian nama nama pegawai company cipta adil yang tertera.", 40, doc.autoTable.previous.finalY + 20);
+
+        // number page
+        let pageCount = doc.internal.getNumberOfPages();
+        let pageHeight = doc.internal.pageSize.getHeight();
+        for (let i = 1; i <= pageCount; i++) {
+          doc.setPage(i);
+
+          doc.setFontSize(10);
+          doc.setFont(undefined, "normal");
+          doc.text("" + i, pageWidth - 35, pageHeight - 15);
+        }
+
+        window.open(doc.output("bloburl"), "_blank"); // tab baru
+        // doc.save("List-Pegawai.pdf"); // auto download pdf
       }
     }
   };
@@ -105,7 +120,6 @@ export default function Index() {
 
   useEffect(() => {
     getAllData();
-    getBase64Image(`http://localhost:3000/karyawan.png`);
     isi();
   }, [data]);
 
